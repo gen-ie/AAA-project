929c39f2579b3b0d5a11cf6a72f05652feef76cf
gen-ie <caraangenevie@gmail.com>
2022-09-17 13:39:07 +0800

    updated with winning conditions

--- game.py
+++ game.py
@@ -1,2 +1,7 @@
 def simulation():
     '''runs the game'''
+    '''
+    if blue can no longer act (lost all energy), red wins
+    if red can no longer act (lost all followers), blue wins
+    '''
+    
