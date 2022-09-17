c3ef76d6bf247c30ab76764ff233208372efbba7
gen-ie <caraangenevie@gmail.com>
2022-09-17 15:00:53 +0800

    added pseudocode

--- agents/blue.py
+++ agents/blue.py
@@ -1,2 +1,8 @@
 class blue:
     '''actions for blue agent'''
+
+    '''message = chosen by player 
+    ask the player if they want to message deployed directly or via gray agent
+    
+    deploy_gray(message)
+    direct(message)'''
