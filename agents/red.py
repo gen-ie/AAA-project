2cdf1496498a7fed53b875513f7a59bbbfc31a49
gen-ie <caraangenevie@gmail.com>
2022-09-17 17:01:25 +0800

    updated with pseudocode

--- agents/red.py
+++ agents/red.py
@@ -1,2 +1,25 @@
+class propaganda:
+    def __init__(self, interval, potency, message):
+        self.interval = interval
+        self.potency = potency
+        self.message = message
+
 class red:
     '''actions for red agent'''
+    def __init__(self, uncertainty):
+        self.uncertainty = uncertainty
+    
+    def spread_misinformation(graph, agent, message):
+        '''
+        for green in nodes (loop through all the nodes):
+            #effectiveness - if random number is smaller than uncertainty value of agent, spread message to them 
+            eff = round(random.uniform(0.7, 1), 2)
+
+            if eff < agent:
+                #change the uncertainty of node
+                green.uncertainty = green.uncertainty + message.potency
+                if green.uncertainty > 1:
+                    green.uncertainty = 1
+            else:
+                do nothing, move to next node
+        '''
