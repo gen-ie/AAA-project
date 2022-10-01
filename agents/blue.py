39fa3c464ee05e660dfd0e6f4995725f1e8fb2ca
Daniel Loo <loodaniel06@gmail.com>
2022-10-01 14:49:33 +0800

    bluer

--- agents/blue.py
+++ agents/blue.py
@@ -1,7 +1,24 @@
 class blue:
-    '''actions for blue agent'''
+    '''actions for blue agent
+    initialise
+        blue = BlueAgent(energy)
+        action : select either deploy gray agent of directly from blue
+        if (direct)
+            if(countmessage.energy >= energy)
+                print("insufficient energy, your game loss if this message is chosen")
+                if (continue chosen)
+                    #red is winner
+                    winner = red
+                    game end() #return winner and final state(?)
+                else
+                    back to action
+            counterpropaganda(population, countermessage) #interact with all green population
+            energy -= countermessage.energycost
+        else
+            deploy_gray_agent(message, countermessage, probability) #message from red, countermessage from blue, chance of grey being red/blue
+ 
 
-    '''message = chosen by player 
+    message = chosen by player 
     ask the player if they want to message deployed directly or via gray agent
     
     deploy_gray(message)
