03776601e6c9aa0011ddf5ee714ad0bd3ebf64ef
gen-ie <caraangenevie@gmail.com>
2022-10-02 20:25:36 +0800

    added red_player() function

--- agents/red.py
+++ agents/red.py
@@ -1,27 +1,82 @@
+import random
+
 class propaganda:
-    def __init__(self, interval, potency, message):
+    def __init__(self, interval, potency):
         self.interval = interval
         self.potency = potency
-        self.message = message
 
 class red:
     '''actions for red agent'''
     def __init__(self, uncertainty):
         self.uncertainty = uncertainty
     
-    def spread_misinformation(array_green, agent, message):
-        '''
-        for green in nodes (loop through all the nodes):
+    def spread_misinformation(self, array_green, message):
+        for green in array_green:
             #effectiveness - if random number is smaller than uncertainty value of agent, spread message to them 
             eff = round(random.uniform(-0.7, -1), 2)
 
-            if eff > agent:
+            if eff < green.uncertainty:
                 #change the uncertainty of node
                 green.uncertainty = green.uncertainty + message.potency
-                if green.uncertainty > 1:
-                    green.uncertainty = 1
-            else:
-                do nothing, move to next node
-        '''
+                if green.uncertainty < -1:
+                    green.uncertainty = -1
 
-    def minimax():
+    def num_followers(message, green_nodes):
+        followers = 0
+        max_interval = message.max_inteval
+        for g in green_nodes:
+            if g.uncertainty < max_interval:
+                followers += 1
+        return followers
+
+    def red_player(self, green_nodes):
+        print("You have a choice of five types of messages to send out:\n")
+
+        print("1: Propaganda level 1 (name under construction) - tame message. Can affect most people (uncertainty below 0.8)") 
+        print("2: Propaganda level 2 (name under construction) - moderately tame message. Can affect a lot of people (uncertainty below 0.6)") 
+        print("3: Propaganda level 3 (name under construction) - moderately effective message. Can affect some people (uncertainty below 0.4)") 
+        print("4: Propaganda level 4 (name under construction) - highly effective message. Can affect only a handful of certain people (uncertainty below 0.2)") 
+        print("5: Propaganda level 5 (name under construction) - heavily potent message. Can only affect uncertain people (uncertainty of below 0)") 
+        print("\n")
+
+        print("Current number of followers: ")
+
+        while True:
+            message_choice = input("Please type the number of your choice (between 1 to 5): ")
+            if message_choice >= 1 and message_choice <= 5:
+                break
+            print("Invalid message type.\n")
+        
+        max_interval = 0
+        potency = 0
+         
+        if int(message_choice) == 1:
+            max_interval = 0.8
+            potency = -0.05
+        elif int(message_choice) == 2:
+            max_interval = 0.6
+            potency = -0.1
+        elif int(message_choice) == 3:
+            max_interval = 0.4
+            potency = -0.15
+        elif int(message_choice) == 4:
+            max_interval = 0.2
+            potency = -0.2
+        elif int(message_choice) == 5:
+            max_interval = 0
+            potency = -0.25
+        
+        # create message
+        message = propaganda(max_interval, potency)
+        # number of interacted followers
+        num_follow = self.num_followers(message, green_nodes)
+        # spread message
+        self.spread_misinformation(green_nodes, message)
+
+        print(f"You have interacted with {num_follow} nodes")
+
+        
+    def red_ai(green_nodes):
+        return 1
+
+
