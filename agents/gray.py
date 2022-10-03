8e7b152542e0476af420b31ad984853505ad45c9
Ishtashii <23116667@student.uwa.edu.au>
2022-10-04 06:31:18 +0800

    only a few changes, will add more soon

--- agents/gray.py
+++ agents/gray.py
@@ -1,2 +1,15 @@
+from random import randint
 class gray:
     '''subclass of blue / actions of gray agent'''
+    #number of gray nodes is expressed as PERCENTAGE in a parameter
+    number = 0 #fix this later by adding parameter that allows the player/similuation to determin amt
+    gray_type = randint(0,1)
+    if gray_type == 0:
+    #red gets to deploy a campaign WITHOUT red team losing followers
+
+        print("gray is red")
+        
+    elif gray_type == 1:
+        print("gray is blue")
+    #blue gets another turnh without losing life, lets gray agent out into network
+    #gray agents know the amount of people who do/dont want to vote
