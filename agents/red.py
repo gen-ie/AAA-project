4c444e89c6f12a45625258a5e9325c0194ae04d2
gen-ie <caraangenevie@gmail.com>
2022-10-08 23:02:07 +0800

    changed max_interval to persuasiveness_dcrease

--- agents/red.py
+++ agents/red.py
@@ -1,8 +1,14 @@
+from functools import update_wrapper
 import random
 
 class propaganda:
-    def __init__(self, interval, potency, type):
-        self.interval = interval
+    def __init__(self, follower_lose, potency, type):
+        '''
+        follower_lose = how much would the uncertainty of red will shift to the RIGHT (becomes more positive)after spreading the message; the more right on the scale red agent is, the less persuasive it is
+        potency = how much the uncertainty of green nodes will change after the propaganda has been spread; aims to shift the nodes LEFT 
+        type = displays the level of message
+        '''
+        self.follower_lose = follower_lose
         self.potency = potency
         self.type = type
 
@@ -13,23 +19,30 @@ class red:
         self.uncertainty = uncertainty
     
     def spread_misinformation(self, array_green, message):
+        num_interact = 0
         for green in array_green:
-            #effectiveness - if random number is smaller than uncertainty value of agent, spread message to them 
-            # eff = round(random.uniform(-0.7, -1), 2)
-
-            if message.max_interval > green.uncertainty:
+            # to determine which nodes red can interact with: take the absolute value of red as maximum uncertainty red can interact with
+            # example: let red have an uncertainty of -0.91; taking the absolute value, red can interact with green nodes with uncertainty 0.91 and below
+            # red cannot gain back its persuasive power 
+            if abs(self.uncertainty) >= green.uncertainty:
+                num_interact += 1
                 #change the uncertainty of node
-                green.uncertainty = green.uncertainty + message.potency
+                green.uncertainty +=  message.potency
                 if green.uncertainty < -1:
                     green.uncertainty = -1
+            # check updated opinion
+            if green.uncertainty <= 0:
+                green.opinion = 0
+        # decrease red's persuasive power
+        self.uncertainty += message.follower_lose
+        return array_green, num_interact
 
-    def num_followers(self, message, green_nodes):
-        followers = 0
-        max_interval = message.max_interval
-        for g in green_nodes:
-            if g.uncertainty < max_interval:
-                followers += 1
-        return followers
+    # def num_followers(self, green_nodes):
+    #     followers = 0
+    #     for g in green_nodes:
+    #         if g.uncertainty <= abs(self.uncertainty):
+    #             followers += 1
+    #     return followers
 
     def red_player(self, green_nodes):
         print("You have a choice of five types of messages to send out:\n")
@@ -45,45 +58,46 @@ class red:
 
         while True:
             message_choice = input("Please type the number of your choice (between 1 to 5): ")
-            if message_choice >= 1 and message_choice <= 5:
+            if int(message_choice) >= 1 and int(message_choice) <= 5:
                 break
             print("Invalid message type.\n")
         
-        max_interval = 0
+        persuasiveness_decrease = 0
         potency = 0
         type = ""
          
         if int(message_choice) == 1:
-            max_interval = 0.8
+            persuasiveness_decrease = 0.025 
             potency = -0.05
             type = "Speech of Patriotism"
         elif int(message_choice) == 2:
-            max_interval = 0.6
+            persuasiveness_decrease = 0.05 
             potency = -0.1
             type = "Propaganda"
         elif int(message_choice) == 3:
-            max_interval = 0.4
+            persuasiveness_decrease = 0.075 
             potency = -0.15
             type = "Conspiracy"
         elif int(message_choice) == 4:
-            max_interval = 0.2
+            persuasiveness_decrease = 0.1
             potency = -0.2
             type = "Fake News"
         elif int(message_choice) == 5:
-            max_interval = 0
+            persuasiveness_decrease = 0.125 
             potency = -0.25
             type = "Fear mongering"
         
         # create message
-        message = propaganda(max_interval, potency, type)
+        message = propaganda(persuasiveness_decrease, potency, type)
         print(f"You have chosen {message.type}!\n")
-        # number of interacted followers
-        num_follow = self.num_followers(message, green_nodes)
 
         # spread message
-        self.spread_misinformation(green_nodes, message)
-        print(f"You have interacted with {num_follow} nodes")
+        updated_nodes, num_interact = self.spread_misinformation(green_nodes, message)
+        print(f"You have interacted with {num_interact} nodes\n\n")
         # print stats of overall opinion
+        for u in updated_nodes:
+            print(u.uncertainty, u.opinion)
+        return updated_nodes
  
     def red_ai(green_nodes):
         # minimax
